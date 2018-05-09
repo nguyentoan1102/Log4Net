1d46181c02fd358d15148ae36de12d897f849df8
nguyentoan1102 <longtinh12@gmail.com>
2018-05-10 01:27:22 +0700

    Wire Log for .NET Application

---
+++ Log4netTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4netTutorial
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = LogHeoper.GetLogger();
+
+        private static void Main(string[] args)
+        {
+            // ImplementLoggingFuntion();
+            Console.WriteLine("Hello world");
+            log.Error("This is my error message");
+
+            Console.ReadLine();
+        }
+    }
+}
